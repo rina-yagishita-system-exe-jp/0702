1ba588e8e9b8daed2b8d8b5e3da6504267b88f44
Rina Yagishita <rina.yagishita@system-exe.jp>
2025-07-04 16:07:45 +0900

    Add generateStaticParams for dynamic route

--- udon-shop/src/app/products/[id]/page.tsx
+++ udon-shop/src/app/products/[id]/page.tsx
@@ -1,11 +1,18 @@
 import { notFound } from 'next/navigation'
-import { getProduct } from '@/lib/data-provider'
+import { getProduct, getProducts } from '@/lib/data-provider'
 import ProductDetailClient from './ProductDetailClient'
 
 interface ProductDetailPageProps {
   params: Promise<{
     id: string
   }>
+}
+
+export async function generateStaticParams() {
+  const products = await getProducts()
+  return products.map((product) => ({
+    id: product.id,
+  }))
 }
 
 export default async function ProductDetailPage({ params }: ProductDetailPageProps) {
